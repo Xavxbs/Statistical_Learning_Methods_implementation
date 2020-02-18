4e2c067902b3b6daf557d8b173ffeb1176fe5c5a
Xavxbs <xavier_xbs@qq.com>
2020-02-18 18:54:59 +0800

    修改bug

--- KNN/main.py
+++ KNN/main.py
@@ -13,10 +13,6 @@ k-近邻算法实现
 具体使用kd树实现了查找，适合训练实例数远大于空间维度时的查找，平均计算复杂度为O（logN）（N为维度）
 当空间维度接近训练实例数时，kd树查找的效率会接近线性扫描
 使用scikit-learn内置的鸢尾花数据集
-训练集数量 480
-测试集数量 120
--------------------------------
-测试集正确率：69.33%
 '''
 def load_data():
     '''
@@ -33,9 +29,9 @@ if __name__ == '__main__':
     #读取数据
     data, labels = load_data()
     #打乱数据并分割数据集
-    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.5, random_state=10)
+    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=11)
     #设定最近参与多数表决的节点数量
-    k = 2
+    k =1
     #创建k-近邻对象
     model = KNN(x_train,y_train,k)
     #对测试集进行测试
